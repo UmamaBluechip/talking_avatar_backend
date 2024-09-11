4b3f8d2f774387524c48911acdd711ae158f0b0b
UmamaBluechip <umamabluechiptech@gmail.com>
2024-09-11 18:05:18 +0530

    updated index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 var textToSpeech = require('../helpers/tts');
 
+/* GET root route */
+router.get('/', function(req, res, next) {
+  res.send('Backend is running!');
+});
+
+
 /* GET home page. */
 router.post('/talk', function(req, res, next) {
 
